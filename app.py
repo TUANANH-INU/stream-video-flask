2f0404bc64cdce902b57a0d9f8a0ac88d668b8d4
TUAN-ANH <72487483+TUANANH-INU@users.noreply.github.com>
2022-01-20 15:39:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,9 @@
 from importlib import import_module
 import os
 from flask import Flask, render_template, Response
-
 from camera_opencv import Camera
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -19,13 +16,10 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
 @app.route('/video_feed1')
 def video_feed1():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True, port=8888)
